2defc61b085026c8c400f436a0e2a036a264896f
Aatu Riuttamäki <aatu@idleplaythings.com>
2014-12-14 20:53:12 +0200

    refactor battlenode

--- monitor/src/app/battlEyeClient.js
+++ monitor/src/app/battlEyeClient.js
@@ -3,43 +3,49 @@ var BattleNode = require('battle-node');
 module.exports = function(){
   'use strict';
 
-  function BattlEyeClient() {
-  }
+  function BattlEyeClient() {}
 
-  BattlEyeClient.prototype.lockServer = function(config){
+  BattlEyeClient.prototype.login  = function(config, callback){
     var battleNode = new BattleNode(config);
     battleNode.login();
     battleNode.on(
       'login',
       function(error, success) {
-        console.log("server locked");
-        battleNode.sendCommand('#lock', function() {});
+        if (error){
+          throw new Error("Unable to log in to battle eye");
+        }
+
+        if (success == false) {
+          throw new Error("Unable to log in to battle eye (success false)");
+        }
+
+        callback(battleNode);
       }
     );
+  });
+
+  BattlEyeClient.prototype.lockServer = function(config){
+    this.login(config, function(battleNode){
+      battleNode.sendCommand('#lock', function() {
+        console.log("server locked");
+      });
+    });
   };
 
   BattlEyeClient.prototype.kickPlayer = function(config, uid){
-    var battleNode = new BattleNode(config);
-    battleNode.login();
-    battleNode.on(
-      'login',
-      function(error, success) {
+    this.login(config, function(battleNode){
+      battleNode.sendCommand('#kick '+ uid, function() {
         console.log("player " + uid + " kicked");
-        battleNode.sendCommand('#kick '+ uid, function() {});
-      }
-    );
+      });
+    });
   };
 
   BattlEyeClient.prototype.shutDownServer = function(config, uid){
-    var battleNode = new BattleNode(config);
-    battleNode.login();
-    battleNode.on(
-      'login',
-      function(error, success) {
+    this.login(config, function(battleNode){
+      battleNode.sendCommand('#shutdown', function() {
         console.log("server shutdown");
-        battleNode.sendCommand('#shutdown', function() {});
-      }
-    );
+      });
+    });
   };
 
   return new BattlEyeClient();

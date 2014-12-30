1e5b0530b94692a3b28885fccf773de69bb42c92
Aatu Riuttamäki <aatu@idleplaythings.com>
2014-12-30 20:06:47 +0200

    Default for starting location

--- monitor/src/domain/squad.js
+++ monitor/src/domain/squad.js
@@ -5,7 +5,7 @@ function Squad(args) {
   this._id = args._id;
   this.companyId = args.companyId;
   this.steamIds = args.steamIds;
-  this.startingLocation = args.startingLocation;
+  this.startingLocation = args.startingLocation || {x:10000, y:10000};
   this.objectives = args.objectives || ['SUPPLY'];
   this.locked = args.locked;
 }

73ff3717a7e21f28fea7c75f22e64aa742e9165a
Aatu Riuttamäki <aatu@idleplaythings.com>
2014-12-30 19:14:42 +0200

    debug

--- monitor/src/app/monitor.js
+++ monitor/src/app/monitor.js
@@ -18,9 +18,7 @@ Monitor.prototype.start = function() {
 Monitor.prototype._startArma = function(){
   var location = this._config.arma.location;
   var command = location + "/arma3server -config=server.cfg -sock_host=::1 -sock_port=1337 -mod=@ikrs;";
-  var options = {
-    cwd: location
-  };
+  var options = {};
 
   child_process.exec(command, [options], function (error, stdout, stderr) {
     console.log('stdout: ' + stdout);

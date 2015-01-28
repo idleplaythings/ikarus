74e3cea5c2e60f8e121c9f3e21a1fc8328419930
Aatu <aatu.riuttamaki@gmail.com>
2015-01-28 04:26:13 +0200

    Add mods list to mission

---
+++ mod/signer.js
@@ -0,0 +1,40 @@
+var fs = require('fs');
+var exec = require('child_process').exec;
+
+var sign = process.argv[2];
+var key = process.argv[3];
+var folder = process.argv[4];
+
+if (! folder || ! sign || ! key) {
+	console.log("Usage node signer.js (path to DSSignFile.exe) (path to private key) (path to pbo folder)");
+	process.exit();
+}
+
+var files = fs.readdirSync(folder);
+
+files.filter(function(filename) {
+	if (! filename) {
+		return false;
+	}
+
+	return filename.indexOf('.pbo') > -1;
+}).forEach(function(pbo) {
+
+	var path = folder + "/" + pbo;
+	if (pbo.indexOf('.bisign') > -1) {
+		fs.unlinkSync(path);
+	} else {
+		var command = '"'+sign + '" "' + key + '" "' + path + '"';
+		//console.log(command);
+		
+		exec(command,
+		  function (error, stdout, stderr) {
+		    if (error !== null) {
+		      console.log('exec error: ' + error);
+		    }
+		});
+		
+	}
+	
+	console.log(pbo);
+});

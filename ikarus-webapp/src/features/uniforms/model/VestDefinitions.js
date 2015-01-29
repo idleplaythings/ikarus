16a507a186de4a744f6f4cf4a446a64d5b0ef303
Aatu <aatu.riuttamaki@gmail.com>
2015-01-29 18:56:39 +0200

    Parse vests and headgear

---
+++ ikarus-webapp/src/features/uniforms/model/VestDefinitions.js
@@ -0,0 +1,20 @@
+var vests = [
+	{ classes: ["V_BandollierB_khk"], name: "Slash Bandolier (Khaki)"},
+	{ classes: ["V_BandollierB_cbr"], name: "Slash Bandolier (Coyote)"},
+	{ classes: ["V_BandollierB_rgr"], name: "Slash Bandolier (Green)"},
+	{ classes: ["V_BandollierB_blk"], name: "Slash Bandolier (Black)"},
+	{ classes: ["V_BandollierB_oli"], name: "Slash Bandolier (Olive)"},
+	{ classes: ["V_Chestrig_khk"], name: "Chest Rig (Khaki)"},
+	{ classes: ["V_Chestrig_rgr"], name: "Chest Rig (Green)"},
+	{ classes: ["V_Chestrig_blk"], name: "Fighter Chestrig (Black)"},
+	{ classes: ["V_Chestrig_oli"], name: "Fighter Chestrig (Olive)"},
+	{ classes: ["V_I_G_resistanceLeader_F"], name: "Tactical Vest (Stavrou)"},
+	{ classes: ["V_Press_F"], name: "Vest (Press)"},
+	{ classes: ["V_Rangemaster_belt"], name: "Rangemaster Belt"},
+	{ classes: ["V_HarnessO_brn"], name: "LBV Harness"},
+	{ classes: ["V_HarnessOGL_brn"], name: "LBV Grenadier Harness"},
+	{ classes: ["V_HarnessO_gry"], name: "LBV Harness (Gray)"},
+	{ classes: ["V_HarnessOGL_gry"], name: "LBV Grenadier Harness (Gray)"},
+	{ classes: ["V_HarnessOSpec_brn"], name: "ELBV Harness"},
+	{ classes: ["V_HarnessOSpec_gry"], name: "ELBV Harness (Gray)"}
+];
--- mission/ikarus.Altis/helpers/findUniforms.sqf
+++ mission/ikarus.Altis/helpers/findUniforms.sqf
@@ -1,7 +1,9 @@
 //To find className and displayName of uniforms
-
+//uniforms: Uniform_Base
+//headgead: H_HelmetB
+//vests: Vest_Camo_Base, Vest_NoCamo_Base
 _data = []; 
-_uniforms = "('Uniform_Base' in ([_x, true ] call BIS_fnc_returnParents))" configClasses (configFile >> "CfgWeapons"); 
+_uniforms = "('Vest_NoCamo_Base' in ([_x, true ] call BIS_fnc_returnParents))" configClasses (configFile >> "CfgWeapons"); 
 
 {
  	_entries = configProperties [

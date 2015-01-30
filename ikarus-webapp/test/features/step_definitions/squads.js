e846b2221176d89b099283bac4cdd1c518ae7349
Petri Koivula <pkjedi@iki.fi>
2015-01-30 22:49:15 +0200

    cs fix

--- ikarus-webapp/test/features/step_definitions/squads.js
+++ ikarus-webapp/test/features/step_definitions/squads.js
@@ -28,11 +28,11 @@ var squadsStepDefinitions = function () {
   this.When(/^I create a squad$/, function (callback) {
     var app = this.app;
     this.app.callCreateSquad()()
-        .finally(function() {
-          app.callTestingEvaluateSquads()()
-              .finally(callback)
-              .catch(this.handleError);
-        })
+      .finally(function() {
+        app.callTestingEvaluateSquads()()
+          .finally(callback)
+          .catch(this.handleError);
+      })
       .catch(this.handleError);
   });
 

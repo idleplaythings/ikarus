db701ad6aa5656f383d12fea60e5c3a147034782
Aatu Riuttamäki <aatu@idleplaythings.com>
2015-02-03 11:08:16 +0200

    add limit to player sub

--- ikarus-webapp/src/features/players/server/publish.js
+++ ikarus-webapp/src/features/players/server/publish.js
@@ -23,13 +23,16 @@ Meteor.publish('SearchPlayers', function(searchString) {
 
   var reg = new RegExp(searchString, 'i');
 
-  return Meteor.users.find({
+  return Meteor.users.find(
+    {
       'services.steam.username': reg
-    }, {
+    },
+    {
       fields: {
         'services.steam': 1,
         'companyId': 1,
         'invites': 1
-      }
+      },
+      limit: 20
     });
 });

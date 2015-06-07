664a442a6d30451d8d91f7ad438689fb2cedefc1
Aatu Riuttamäki <aatu@idleplaythings.com>
2015-06-07 18:17:41 +0300

    Guard penalties

--- ikarus-webapp/src/features/objectives/model/ObjectiveGuard.js
+++ ikarus-webapp/src/features/objectives/model/ObjectiveGuard.js
@@ -16,12 +16,37 @@ ObjectiveGuard = function ObjectiveGuard() {
     ],
   };
 
+  this._lootTransformPenalty = {
+    'guard_objective_reward1': [
+      'IKRS_loot_money',
+      'IKRS_loot_money'
+    ]
+  };
+
   this.description = "You will be guarding one of the objective depots."
    + " You will be provided with default equipment, but please take"
    + " other equipment too in case you don't get this mission";
 };
 
+var ObjectiveGuard_PENALTY = "guard_objective_penalty";
+
 ObjectiveGuard.prototype = Object.create(Objective.prototype);
+
+ObjectiveGuard.prototype.getLootTransform = function(loot) {
+  var penaltyCount = loot.filter(function(entry) {
+    return entry == ObjectiveGuard_PENALTY;
+  }).length;
+
+  if (penaltyCount == 1) {
+    return this._lootTransformPenalty;
+  }
+
+  if (penaltyCount > 1) {
+    return [];
+  }
+
+  return this._lootTransform();
+};
 
 ObjectiveGuard.prototype.allowLoot = function() {
   return false;
